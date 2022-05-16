34f7f54d29eaed623676aea16bb13effdd373c3a
Britan1984 <59679168+Britan1984@users.noreply.github.com>
2022-05-16 12:25:16 +0300

    Update Tic-Tac-Toe.kt

--- Tic-Tac-Toe.kt
+++ Tic-Tac-Toe.kt
@@ -1,89 +1,5 @@
 package tictactoe
 
-//
-//    for (i in 0..listWins.lastIndex) {
-//        if (listWins[i] == true) {
-//            lineWin ++
-//            when(i) {
-//                0 -> if (listCells[0] == "X") {
-//                    xWin ++
-//                } else
-//                    if (listCells[0] == "O") {
-//                        oWin ++
-//                    }
-//                1 -> if (listCells[3] == "X") {
-//                    xWin ++
-//                } else
-//                    if (listCells[3] == "O") {
-//                        oWin ++
-//                    }
-//                2 -> if (listCells[6] == "X") {
-//                    xWin ++
-//                } else
-//                    if (listCells[6] == "O") {
-//                        oWin ++
-//                    }
-//                3 -> if (listCells[0] == "X") {
-//                    xWin ++
-//                } else
-//                    if (listCells[0] == "O") {
-//                        oWin ++
-//                    }
-//                4 -> if (listCells[1] == "X") {
-//                    xWin ++
-//                } else
-//                    if (listCells[1] == "O") {
-//                        oWin ++
-//                    }
-//                5 -> if (listCells[2] == "X") {
-//                    xWin ++
-//                } else
-//                    if (listCells[2] == "O") {
-//                        oWin ++
-//                    }
-//                6 -> if (listCells[0] == "X") {
-//                    xWin ++
-//                } else
-//                    if (listCells[0] == "O") {
-//                        oWin ++
-//                    }
-//                7 -> if (listCells[2] == "X") {
-//                    xWin ++
-//                } else
-//                    if (listCells[2] == "O") {
-//                        oWin ++
-//                    }
-//            }
-//        }
-//    }
-//    if (xPoint > oPoint + 1 || oPoint > xPoint + 1) {
-//        println("Impossible")
-//        return
-//    }
-//    for (i in 0..listCells.lastIndex) {
-//        if (listCells[i].equals("_") && xWin == 0 && listCells[i].equals("_") && oWin == 0) {
-//            println("Game not finished")
-//            return
-//        }
-//    }
-//    if (lineWin > 1) {
-//        println("Impossible")
-//        return
-//    } else
-//        if (xWin > 0) {
-//            println("X wins")
-//            return
-//        } else
-//            if (oWin > 0) {
-//                println("O wins")
-//                return
-//            } else
-//                if (xWin == 0 && oWin == 0) {
-//                    println("Draw")
-//                    return
-//                }
-//}
-
 fun printer(list1: MutableList<Char>, list2: MutableList<Char>, list3: MutableList<Char>){
     println("---------")
     println("| ${list1[0]} ${list1[1]} ${list1[2]} |")
@@ -122,10 +38,32 @@ fun oWins(list1: MutableList<Char>, list2: MutableList<Char>, list3: MutableList
     return oWin
 }
 
+fun pustMesto(list1: MutableList<Char>, list2: MutableList<Char>, list3: MutableList<Char>):Int {
+    var p = 0
+    for (i in list1) {
+        if (i == ' ') {
+            p = 1
+        }
+    }
+    for (i in list2) {
+        if (i == ' ') {
+            p = 1
+        }
+    }
+    for (i in list3) {
+        if (i == ' ') {
+            p = 1
+        }
+    }
+    return p
+}
+
 fun proverka(list1: MutableList<Char>, list2: MutableList<Char>, list3: MutableList<Char>):String {
     val x = xWins(list1, list2, list3)
     val o = oWins(list1, list2, list3)
-    if (x > 0 && o > 0) {
+    val p = pustMesto(list1, list2, list3)
+    val xop = x+o+p
+    if (x > 0 && o > 0 || xop == 0) {
         println("Draw")
         return "STOP"
     } else
@@ -140,71 +78,119 @@ fun proverka(list1: MutableList<Char>, list2: MutableList<Char>, list3: MutableL
     return ""
 }
 
-fun main() {
-
-    print("Enter cells: ")
-    val enterCells = readln()
-    var xPoint = 0
-    var oPoint = 0
-    var lineWin = 0
-    var xWin = 0
-    var oWin = 0
-    for (i in enterCells) {
-        if (i == 'X') ++xPoint
-        if (i == 'O') ++oPoint
+fun hodX(x1: String, y1: String, list1: MutableList<Char>, list2: MutableList<Char>, list3: MutableList<Char>):Boolean {
+    if (x1.toInt() >= 1 && x1.toInt() <= 3 && y1.toInt() >= 1 && y1.toInt() <= 3) { //проверка на числа от 1 до 3
+        val tempCell: Char  //значение в ячейке в которое планируем вносить Х
+        if (x1.toInt() == 1) {
+            tempCell = list1[y1.toInt() - 1]    //значение из первой строки если первое число = 1
+        } else
+            if (x1.toInt() == 2) {  //значение из второй строки если первое число = 2
+                tempCell = list2[y1.toInt() - 1]
+            } else tempCell = list3[y1.toInt() - 1] //значение из третьей строки если первое число = 3
+        if (tempCell != ' ') {  //если число в строке не пусто
+            println("This cell is occupied! Choose another one!")
+        } else
+            if (x1.toInt() == 1) {  //если пусто и первое число = 1 подставляем на пустое место Х
+                list1.removeAt(y1.toInt()-1)
+                list1.add(y1.toInt()-1, 'X')
+                printer(list1, list2, list3)
+                if (proverka(list1, list2, list3) == "STOP") {
+                    System.exit(0)
+                }
+                return false
+            } else
+                if (x1.toInt() == 2) {  //если пусто и первое число = 2 подставляем на пустое место Х
+                    list2.removeAt(y1.toInt()-1)
+                    list2.add(y1.toInt()-1, 'X')
+                    printer(list1, list2, list3)
+                    if (proverka(list1, list2, list3) == "STOP") {
+                        System.exit(0)
+                    }
+                    return false
+                } else {    //если пусто и первое число = 3 подставляем на пустое место Х
+                    list3.removeAt(y1.toInt()-1)
+                    list3.add(y1.toInt()-1, 'X')
+                    printer(list1, list2, list3)
+                    if (proverka(list1, list2, list3) == "STOP") {
+                        System.exit(0)
+                    }
+                    return false
+                }
     }
-    val list1 = mutableListOf(enterCells[0], enterCells[1], enterCells[2])  //первая строка
-    val list2 = mutableListOf(enterCells[3], enterCells[4], enterCells[5])  //вторая строка
-    val list3 = mutableListOf(enterCells[6], enterCells[7], enterCells[8])  //третья строка
+    else println("Coordinates should be from 1 to 3!")
+    return true
+}
+
+fun hodO(x2: String, y2: String, list1: MutableList<Char>, list2: MutableList<Char>, list3: MutableList<Char>):Boolean {
+    if (x2.toInt() >= 1 && x2.toInt() <= 3 && y2.toInt() >= 1 && y2.toInt() <= 3) { //проверка на числа от 1 до 3
+        val tempCell: Char  //значение в ячейке в которое планируем вносить O
+        if (x2.toInt() == 1) {
+            tempCell = list1[y2.toInt() - 1]    //значение из первой строки если первое число = 1
+        } else
+            if (x2.toInt() == 2) {  //значение из второй строки если первое число = 2
+                tempCell = list2[y2.toInt() - 1]
+            } else tempCell = list3[y2.toInt() - 1] //значение из третьей строки если первое число = 3
+        if (tempCell != ' ') {  //если число в строке не пусто
+            println("This cell is occupied! Choose another one!")
+        } else
+            if (x2.toInt() == 1) {  //если пусто и первое число = 1 подставляем на пустое место Х
+                list1.removeAt(y2.toInt()-1)
+                list1.add(y2.toInt()-1, 'O')
+                printer(list1, list2, list3)
+                if (proverka(list1, list2, list3) == "STOP") {
+                    System.exit(0)
+                }
+                return false
+            } else
+                if (x2.toInt() == 2) {  //если пусто и первое число = 2 подставляем на пустое место Х
+                    list2.removeAt(y2.toInt()-1)
+                    list2.add(y2.toInt()-1, 'O')
+                    printer(list1, list2, list3)
+                    if (proverka(list1, list2, list3) == "STOP") {
+                        System.exit(0)
+                    }
+                    return false
+                } else {    //если пусто и первое число = 3 подставляем на пустое место Х
+                    list3.removeAt(y2.toInt()-1)
+                    list3.add(y2.toInt()-1, 'O')
+                    printer(list1, list2, list3)
+                    if (proverka(list1, list2, list3) == "STOP") {
+                        System.exit(0)
+                    }
+                    return false
+                }
+    }
+    else println("Coordinates should be from 1 to 3!")
+    return true
+}
+
+fun main() {
+    val list1 = mutableListOf(' ', ' ', ' ')  //первая строка
+    val list2 = mutableListOf(' ', ' ', ' ')  //вторая строка
+    val list3 = mutableListOf(' ', ' ', ' ')  //третья строка
     printer(list1, list2, list3) //печатаем сетку
     while (true) {
-//        if (proverka(list1, list2, list3) == "STOP") {
-//            break
-//        }
-        print("Enter the coordinates: ")
-        val (x1, y1) = readln().split(" ")  //координаты добавляемого Х
-        try {
-            if (x1.toInt() >= 1 && x1.toInt() <= 3 && y1.toInt() >= 1 && y1.toInt() <= 3) { //проверка на числа от 1 до 3
-                var tempCell: Char  //значение в ячейке в которое планируем вносить Х
-                if (x1.toInt() == 1) {
-                    tempCell = list1[y1.toInt() - 1]    //значение из первой строки если первое число = 1
-                } else
-                    if (x1.toInt() == 2) {  //значение из второй строки если первое число = 2
-                        tempCell = list2[y1.toInt() - 1]
-                    } else tempCell = list3[y1.toInt() - 1] //значение из третьей строки если первое число = 3
-                if (tempCell != '_') {  //если число в строке не пусто
-                    println("This cell is occupied! Choose another one!")
-                } else
-                    if (x1.toInt() == 1) {  //если пусто и первое число = 1 подставляем на пустое место Х
-                        list1.removeAt(y1.toInt()-1)
-                        list1.add(y1.toInt()-1, 'X')
-                        printer(list1, list2, list3)
-                        break
-//                        if (proverka(list1, list2, list3) == "STOP") {
-//                            break
-//                        }
-                    } else
-                        if (x1.toInt() == 2) {  //если пусто и первое число = 2 подставляем на пустое место Х
-                            list2.removeAt(y1.toInt()-1)
-                            list2.add(y1.toInt()-1, 'X')
-                            printer(list1, list2, list3)
-                            break
-//                            if (proverka(list1, list2, list3) == "STOP") {
-//                                break
-//                            }
-                        } else {    //если пусто и первое число = 3 подставляем на пустое место Х
-                            list3.removeAt(y1.toInt()-1)
-                            list3.add(y1.toInt()-1, 'X')
-                            printer(list1, list2, list3)
-                            break
-//                            if (proverka(list1, list2, list3) == "STOP") {
-//                                break
-//                            }
-                        }
+        while (true) {
+            try {
+                print("Enter the coordinates: ")
+                val (x1, y1) = readln().split(" ")  //координаты добавляемого Х
+                if (hodX(x1, y1, list1, list2, list3) == false) {
+                    break
+                }
+            } catch (e: NumberFormatException) {
+                println("You should enter numbers!")
             }
-            else println("Coordinates should be from 1 to 3!")
-        } catch (e: NumberFormatException) {
-            println("You should enter numbers!")
+        }
+        while (true) {
+            try {
+                print("Enter the coordinates: ")
+                val (x2, y2) = readln().split(" ")  //координаты добавляемого O
+                if (hodO(x2, y2, list1, list2, list3) == false) {
+                    break
+                }
+            } catch (e: NumberFormatException) {
+                println("You should enter numbers!")
+            }
         }
     }
 }
