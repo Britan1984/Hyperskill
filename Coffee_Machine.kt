168f9a55c51ca5d03e9ed0507c09a886802c7e07
Britan1984 <59679168+Britan1984@users.noreply.github.com>
2022-05-24 21:48:51 +0300

    Update Coffee_Machine.kt

--- Coffee_Machine.kt
+++ Coffee_Machine.kt
@@ -1,6 +1,7 @@
 package machine
 
 import kotlin.math.ceil
+import kotlin.math.floor
 import kotlin.math.min
 
 var cupsCoffee = 0
@@ -12,35 +13,37 @@ var coffeeCupsExitTemp = 0
 fun enterComponents() {
     print("Write how many ml of water the coffee machine has: ")
     water = readln().toInt()
-    println("")
     print("Write how many ml of milk the coffee machine has: ")
     milk = readln().toInt()
-    println("")
     print("Write how many grams of coffee beans the coffee machine has: ")
     coffee = readln().toInt()
-    println("")
     print("Write how many cups of coffee you will need: ")
     cupsCoffee = readln().toInt()
-    println("")
 }
 
 fun coffeeCupsExit() {
-    val cupsIfWater = ceil(water / 200.toDouble()).toInt()
-    val cupsIfMilk = ceil(milk / 50.toDouble()).toInt()
-    val cupsIfCofee = ceil(coffee / 15.toDouble()).toInt()
+    val cupsIfWater = floor(water / 200.toDouble()).toInt()
+    val cupsIfMilk = floor(milk / 50.toDouble()).toInt()
+    val cupsIfCofee = floor(coffee / 15.toDouble()).toInt()
     coffeeCupsExitTemp = min(min(cupsIfWater, cupsIfMilk), cupsIfCofee)
     print()
 }
 
 fun print() {
-    if ()
+    if (cupsCoffee == coffeeCupsExitTemp) {
+        println("Yes, I can make that amount of coffee")
+    } else
+        if (cupsCoffee < coffeeCupsExitTemp) {
+            println("Yes, I can make that amount of coffee (and even ${coffeeCupsExitTemp - cupsCoffee} more than that)")
+        } else println("No, I can make only $coffeeCupsExitTemp cups of coffee")
 }
 
 fun main() {
     enterComponents()
-    println("""For $cupsCoffee cups of coffee you will need:
-${cupsCoffee * 200} ml of water
-${cupsCoffee * 50} ml of milk
-${cupsCoffee * 15} g of coffee beans
-    """.trimMargin())
+    coffeeCupsExit()
+//    println("""For $cupsCoffee cups of coffee you will need:
+//${cupsCoffee * 200} ml of water
+//${cupsCoffee * 50} ml of milk
+//${cupsCoffee * 15} g of coffee beans
+//    """.trimMargin())
 }
