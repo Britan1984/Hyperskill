d34ebd85e09e69102c7d999ba90d2e2cbafe6fd4
Britan1984 <59679168+Britan1984@users.noreply.github.com>
2022-11-02 00:26:20 +0300

    Update EncryptDecrypt.kt

--- EncryptDecrypt.kt
+++ EncryptDecrypt.kt
@@ -1,19 +1,33 @@
 package encryptdecrypt
 
-fun main() {
-    val text = readln().toString()       //начальное сообщение
-    val key = readln().toInt()          //Ключ шифрования
-    val z1 = ('a'..'z').joinToString("").toMutableList()        //массив алфавита
-    var description = ""        //шифровонное сообщение
-    var i = 0       //счетчик
-    while (i < text.length) {       //цикл на длинну текста
-        if (z1.contains(text.get(i))) {     //проверка что массив содержит символ из текста
-            if (z1.indexOf(text.get(i)) + key < z1.size) {      //проверка что индекс символа текста + ключ не превышают длинну массива (алфавита)
-                description = description.plus(z1.get(z1.indexOf(text.get(i)) + key)) // шифруем символ добавив к индексу символа ключ и по новому индексу взяли новый символ из массива
-            } else description = description.plus(z1.get(z1.indexOf(text.get(i)) + key - z1.size))  //если индекс символа текста + ключ превышают длинну массива (алфавита) и шифруем его
-        }
-        else description = description.plus(text.get(i))    // записываем символы которые не содержатся в массиве
+fun encrypt(text:CharArray, key:Int){
+    var encryptText =""
+    var i = 0
+    while (i < text.size){
+        encryptText = encryptText.plus(text.get(i)+key)
         i++
     }
-    print(description)      //печатаем шифрованное сообщение
+    print(encryptText)
+}
+
+fun decrypt(text:CharArray, key:Int){
+    var decryptText =""
+    var i = 0
+    while (i < text.size){
+        decryptText = decryptText.plus(text.get(i)-key)
+        i++
+    }
+    print(decryptText)
+}
+
+
+fun main() {
+    val change = readln()
+    val text = readln().toCharArray()      //начальное сообщение
+    val key = readln().toInt()          //Ключ шифрования
+    when (change){
+        "enc" -> encrypt(text, key)
+        "dec" -> decrypt(text, key)
+        else -> print("Ошибка выбора")
+    }
 }
