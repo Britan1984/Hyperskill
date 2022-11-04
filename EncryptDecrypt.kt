50356e375f7a248d8df1c4925f8220e6214db544
Britan1984 <59679168+Britan1984@users.noreply.github.com>
2022-11-05 00:11:53 +0300

    Update EncryptDecrypt.kt

--- EncryptDecrypt.kt
+++ EncryptDecrypt.kt
@@ -1,6 +1,10 @@
 package encryptdecrypt
 
-fun encrypt(text:CharArray, key:Int){
+fun nalichie(argum: String, argsArr: Array<String>): Boolean {
+    return argum in argsArr
+}
+
+fun encrypt(text: Array<Char>, key:Int){
     var encryptText =""
     var i = 0
     while (i < text.size){
@@ -10,7 +14,7 @@ fun encrypt(text:CharArray, key:Int){
     print(encryptText)
 }
 
-fun decrypt(text:CharArray, key:Int){
+fun decrypt(text: Array<Char>, key:Int){
     var decryptText =""
     var i = 0
     while (i < text.size){
@@ -21,13 +25,30 @@ fun decrypt(text:CharArray, key:Int){
 }
 
 
-fun main() {
-    val change = readln()
-    val text = readln().toCharArray()      //начальное сообщение
-    val key = readln().toInt()          //Ключ шифрования
+fun main(args:Array<String>) {
+
+    var change = "enc"
+    var text = ""      //начальное сообщение
+    var chText = emptyArray<Char>()
+    var key = 0          //Ключ шифрования
+    if(nalichie("-mode",args)) {
+        change = args[args.indexOf("-mode") + 1]
+    }
+    if(nalichie("-key",args)){
+        key = args[args.indexOf("-key")+1].toInt()
+    }
+    if(nalichie("-data",args)){
+        text = args[args.indexOf("-data")+1]
+    }
+    var i=0
+    while (i < text.length){
+        chText += text[i]
+        i++
+    }
+
     when (change){
-        "enc" -> encrypt(text, key)
-        "dec" -> decrypt(text, key)
+        "enc" -> encrypt(chText, key)
+        "dec" -> decrypt(chText, key)
         else -> print("Ошибка выбора")
     }
 }
