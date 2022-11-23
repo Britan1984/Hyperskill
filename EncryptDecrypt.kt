a6123d5abcfaa0e7a71e5a8da16c9b1ad029eb61
Britan1984 <59679168+Britan1984@users.noreply.github.com>
2022-11-23 23:48:15 +0300

    Update EncryptDecrypt.kt

--- EncryptDecrypt.kt
+++ EncryptDecrypt.kt
@@ -1,28 +1,31 @@
 package encryptdecrypt
 
+import java.io.File
+
 fun nalichie(argum: String, argsArr: Array<String>): Boolean {
     return argum in argsArr
 }
 
-fun encrypt(text: Array<Char>, key:Int){
+fun encrypt(text: Array<Char>, key:Int):String{
     var encryptText =""
     var i = 0
     while (i < text.size){
         encryptText = encryptText.plus(text.get(i)+key)
         i++
     }
-    print(encryptText)
+    return encryptText
 }
 
-fun decrypt(text: Array<Char>, key:Int){
+fun decrypt(text: Array<Char>, key:Int):String{
     var decryptText =""
     var i = 0
     while (i < text.size){
         decryptText = decryptText.plus(text.get(i)-key)
         i++
     }
-    print(decryptText)
+    return decryptText
 }
+
 
 
 fun main(args:Array<String>) {
@@ -31,6 +34,10 @@ fun main(args:Array<String>) {
     var text = ""      //начальное сообщение
     var chText = emptyArray<Char>()
     var key = 0          //Ключ шифрования
+    var encDecText = ""
+    var inText = ""
+    var outText = ""
+
     if(nalichie("-mode",args)) {
         change = args[args.indexOf("-mode") + 1]
     }
@@ -40,15 +47,29 @@ fun main(args:Array<String>) {
     if(nalichie("-data",args)){
         text = args[args.indexOf("-data")+1]
     }
+    if(nalichie("-in",args)&&text==""){
+        inText= args[args.indexOf("-in") + 1]
+        text = File(inText).readText()
+    }
+    if(nalichie("-out",args)){
+        outText = args[args.indexOf("-out") + 1]
+    }
+
     var i=0
+
     while (i < text.length){
         chText += text[i]
         i++
     }
 
     when (change){
-        "enc" -> encrypt(chText, key)
-        "dec" -> decrypt(chText, key)
-        else -> print("Ошибка выбора")
+        "enc" -> encDecText = encrypt(chText, key)
+        "dec" -> encDecText = decrypt(chText, key)
+        else -> print("Error")
     }
+
+    if (outText==""){
+        println(encDecText)
+    }
+    else File(outText).writeText(encDecText)
 }
