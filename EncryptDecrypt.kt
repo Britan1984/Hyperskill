c4c21d666719919983ecbb97d19e5bdced376959
Britan1984 <59679168+Britan1984@users.noreply.github.com>
2022-11-25 01:04:29 +0300

    Update EncryptDecrypt.kt

--- EncryptDecrypt.kt
+++ EncryptDecrypt.kt
@@ -6,7 +6,7 @@ fun nalichie(argum: String, argsArr: Array<String>): Boolean {
     return argum in argsArr
 }
 
-fun encrypt(text: Array<Char>, key:Int):String{
+fun encryptUni(text: Array<Char>, key:Int):String{
     var encryptText =""
     var i = 0
     while (i < text.size){
@@ -16,12 +16,74 @@ fun encrypt(text: Array<Char>, key:Int):String{
     return encryptText
 }
 
-fun decrypt(text: Array<Char>, key:Int):String{
+fun decryptUni(text: Array<Char>, key:Int):String{
     var decryptText =""
     var i = 0
     while (i < text.size){
         decryptText = decryptText.plus(text.get(i)-key)
         i++
+    }
+    return decryptText
+}
+
+fun encryptShi(text: String, key: Int, mBukv: List<String>, bBukv: List<String>):String{
+    var encryptText =""
+    var i = 0
+    while (i < text.length){
+        if (mBukv.contains<Any>(text.get(i).toString())) {
+            if (mBukv.size-key>=mBukv.indexOf(text.get(i).toString())) {
+                encryptText = encryptText.plus(mBukv[mBukv.indexOf(text.get(i).toString()) + key])
+            }
+            else {
+                encryptText = encryptText.plus(mBukv[mBukv.indexOf(text.get(i).toString()) - mBukv.size + key])
+            }
+            i++
+        }
+
+        else if (bBukv.contains<Any>(text.get(i).toString())) {
+            if (bBukv.size-key>=bBukv.indexOf(text.get(i).toString())) {
+                encryptText = encryptText.plus(bBukv[bBukv.indexOf(text.get(i).toString()) + key])
+            }
+            else {
+                encryptText = encryptText.plus(bBukv[bBukv.indexOf(text.get(i).toString()) - bBukv.size + key])
+            }
+            i++
+        }
+        else {
+            encryptText = encryptText.plus(text[i])
+            i++
+        }
+    }
+    return encryptText
+}
+
+fun decryptShi(text:String, key:Int, mBukv: List<String>, bBukv: List<String>):String{
+    var decryptText =""
+    var i = 0
+    while (i < text.length){
+        if (mBukv.contains<Any>(text.get(i).toString())) {
+            if (mBukv.indexOf(text.get(i).toString())>=key) {
+                decryptText = decryptText.plus(mBukv[mBukv.indexOf(text.get(i).toString()) - key])
+            }
+            else {
+                decryptText = decryptText.plus(mBukv[mBukv.size -key + mBukv.indexOf(text.get(i).toString())])
+            }
+            i++
+        }
+
+        else if (bBukv.contains<Any>(text.get(i).toString())) {
+            if (bBukv.indexOf(text.get(i).toString())>=key) {
+                decryptText = decryptText.plus(bBukv[bBukv.indexOf(text.get(i).toString()) - key])
+            }
+            else {
+                decryptText = decryptText.plus(bBukv[bBukv.size -key + bBukv.indexOf(text.get(i).toString())])
+            }
+            i++
+        }
+        else {
+            decryptText = decryptText.plus(text[i])
+            i++
+        }
     }
     return decryptText
 }
@@ -37,6 +99,10 @@ fun main(args:Array<String>) {
     var encDecText = ""
     var inText = ""
     var outText = ""
+    var algoritm = "shift"
+    var i=0
+    val mBukv = listOf("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+    val bBukv = listOf("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
 
     if(nalichie("-mode",args)) {
         change = args[args.indexOf("-mode") + 1]
@@ -54,18 +120,28 @@ fun main(args:Array<String>) {
     if(nalichie("-out",args)){
         outText = args[args.indexOf("-out") + 1]
     }
+    if (nalichie("-alg",args)){
+        algoritm = args[args.indexOf("-alg") + 1]
+    }
 
-    var i=0
+
 
     while (i < text.length){
         chText += text[i]
         i++
     }
 
-    when (change){
-        "enc" -> encDecText = encrypt(chText, key)
-        "dec" -> encDecText = decrypt(chText, key)
-        else -> print("Error")
+    when (algoritm){
+        "unicode" -> when (change){
+            "enc" -> encDecText = encryptUni(chText, key)
+            "dec" -> encDecText = decryptUni(chText, key)
+            else -> print("Error")
+        }
+        "shift" -> when (change){
+            "enc" -> encDecText = encryptShi(text, key, mBukv, bBukv)
+            "dec" -> encDecText = decryptShi(text, key, mBukv, bBukv)
+            else -> print("Error")
+        }
     }
 
     if (outText==""){
