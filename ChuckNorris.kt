8cdc9df5d767c6458d8bc6ac0c519466ac082760
Britan1984 <59679168+Britan1984@users.noreply.github.com>
2023-07-11 13:36:50 +0300

    Update ChuckNorris.kt

--- ChuckNorris.kt
+++ ChuckNorris.kt
@@ -4,16 +4,40 @@ import java.lang.StringBuilder
 
 fun main() {
     println("Input string:")
-    val text = readln().toCharArray()
+    val text = readln()
+    val textArr = arrayListOf<String>()
+    var hexText = ""
+    for (i in text){
+        textArr.add(i.toString())
+    }
     println("The result:")
-    for (i in text) {
-        var hexChar = StringBuilder().append(Integer.toBinaryString(i.toInt()))
+    for (i in textArr) {
+        var hexChar = StringBuilder().append(Integer.toBinaryString(i.single().code))
         var hexString = hexChar.toString()
         while (true) {
             if (hexString.length < 7) {
                 hexString = "0" + hexString
             } else break
         }
-        println("$i = $hexString")
+        hexText += hexString
+    }
+    var tempHexNum = "2"
+    for (i in hexText){
+        if (tempHexNum.equals("2")) {
+                if(i.toString().equals("1")) print("0 0")
+                else if (i.toString().equals("0")) print("00 0")
+                tempHexNum = i.toString()
+        }
+        else {
+            if (tempHexNum.equals(i.toString())){
+                print("0")
+            }
+            else {
+                if (i.toString().equals("0")) print(" 00 0")
+                else if (i.toString().equals("1")) print(" 0 0")
+            }
+            tempHexNum = i.toString()
+
+        }
     }
 }
